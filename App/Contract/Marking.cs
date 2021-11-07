644640aecf267856c324e3a338d7166313813c2f
Stefan Fjällemark <fjallemark@hotmail.com>
2021-11-07 20:09:35 +0100

    Improved text fit of wagon header.

--- App/Contract/Marking.cs
+++ App/Contract/Marking.cs
@@ -2,6 +2,7 @@
 {
     public class Marking
     {
+        public bool UseMarking { get; set; } = true;
         public string Color1 { get; set; } = "#FFFFFF";
         public string Color2 {  get; set;} = "#FFFFFF";
         public string Color3 { get; set; } = "#FFFFFF";
