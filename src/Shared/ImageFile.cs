a3a3ec329651223ff8b98db28992cba88062384a
Stefan Fjällemark <fjallemark@hotmail.com>
2022-04-05 14:49:34 +0200

    Release 2.1.7, see release note.

--- src/Shared/ImageFile.cs
+++ src/Shared/ImageFile.cs
@@ -4,4 +4,6 @@ public class ImageFile
 	public string? Base64data { get; set; }
 	public string? ContentType { get; set; }
 	public string? FileName { get; set; }
+	public bool IsImage => !string.IsNullOrEmpty(Base64data);
+	public void Clear() => Base64data = null;
 }
