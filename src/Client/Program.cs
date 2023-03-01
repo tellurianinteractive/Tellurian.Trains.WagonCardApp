1566be80bd270eddb03c993ec1309f20146d9548
Stefan Fjällemark <fjallemark@hotmail.com>
2023-03-01 18:17:15 +0100

    Release 2.1.21.

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -2,13 +2,12 @@ global using Tellurian.WagonCardApp.Client;
 global using Tellurian.WagonCardApp.Shared;
 global using Microsoft.Extensions.Localization;
 
-
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
 using BlazorDownloadFile;
 using Tellurian.WagonCardApp.Client.Services;
-
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -18,5 +17,6 @@ builder.Services.AddBlazoredLocalStorage(config => config.JsonSerializerOptions.
 builder.Services.AddBlazorDownloadFile(ServiceLifetime.Scoped);
 builder.Services.AddScoped<IContentService, ContentService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddPWAUpdater();
 
 await builder.Build().RunAsync();
--- src/Shared/PrintOptions.cs
+++ src/Shared/PrintOptions.cs
@@ -5,4 +5,5 @@ public class PrintOptions
     public bool PrintCard { get; set; } = true;
     public bool PrintInventoryCard { get; set; } = true;
     public bool PrintFredSticker { get; set; } = true;
+    public bool PrintExtraFredSticker { get; set ; } = false;
 }
