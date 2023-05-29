ef28b90bdec167b136e2167ea5d3647f2ddc8a7a
Stefan Fjällemark <fjallemark@hotmail.com>
2023-05-29 15:56:41 +0200

    Release 2.1.25. Fix of issue #21.

--- src/Shared/Marking.cs
+++ src/Shared/Marking.cs
@@ -1,8 +1,11 @@
-﻿namespace Tellurian.WagonCardApp.Shared;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Tellurian.WagonCardApp.Shared;
 
 public class Marking
 {
     public bool UseMarking { get; set; } = true;
+    public bool UseColorMarking { get; set; } = false;
     public string Color1 { get; set; } = "#FFFFFF";
     public string Color2 { get; set; } = "#FFFFFF";
     public string Color3 { get; set; } = "#FFFFFF";
@@ -14,11 +17,18 @@ public class Marking
 
 public static class MarkingExtensions
 {
-    public static bool ShowOwnerTextLabel(this Marking? it) => !it.HasInventoryNumber() && !it.HasOwnerIcon();
-    public static bool ShowInventoryNumber(this Marking? it) => !it.HasOwnerIcon() && !it.HasOwnerTextLabel();
-    public static bool ShowOwnerIcon(this Marking? it) => !it.HasInventoryNumber() && !it.HasOwnerTextLabel();
-    public static bool ShowColourMarkings(this Marking? it) => !it.HasInventoryNumber() && !it.HasOwnerIcon() && !it.HasOwnerTextLabel();
+    public static bool ShowColourMarkings(this Marking? it) => it.HasColourMarking();
+    public static bool ShowOwnerTextLabel(this Marking? it) => it.HasTextLabel() && !it.HasColourMarking();
+    public static bool ShowOwnerIcon(this Marking? it) => (it.HasOwnerIcon()|| it.HasOwnerIconHref()) && !it.HasTextLabel() && !it.HasColourMarking();
+    public static bool ShowInventoryNumber(this Marking? it) => it.HasInventoryNumber();
+
+    public static bool EditMarking(this Marking? it) => it is not null;
+
+    private static bool HasColourMarking(this Marking? it) => it?.UseColorMarking == true;
     private static bool HasInventoryNumber(this Marking? it) => !string.IsNullOrWhiteSpace(it?.InventoryNumber);
-    private static bool HasOwnerIcon(this Marking? it) => it?.IconHref?.StartsWith("http") == true;
-    private static bool HasOwnerTextLabel(this Marking? it) => it?.TextLabel.HasValue() == true;
+    private static bool HasTextLabel(this Marking? it) => it?.TextLabel.HasValue() == true;
+
+    public static bool HasOwnerIcon([NotNullWhen(true)] this Marking? it) => it?.Icon is not null ;
+    public static bool HasOwnerIconHref([NotNullWhen(true)] this Marking? it) => it?.IconHref?.StartsWith("http") == true;
+    public static string BackgroundColor(this Marking? it) => it.ShowColourMarkings() || it.ShowOwnerIcon() || it.ShowOwnerTextLabel() ? "white" : it?.Color1 ?? "white";
 }
