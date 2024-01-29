438fe6f779ecb4613c426bbd39ef57476839b858
Stefan Fjällemark <fjallemark@hotmail.com>
2024-01-29 15:56:06 +0100

    Release 2.1.29.

--- src/Client/Extensions/StringExtensions.cs
+++ src/Client/Extensions/StringExtensions.cs
@@ -1,9 +1,12 @@
-﻿namespace Tellurian.WagonCardApp.Client.Extensions;
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+
+namespace Tellurian.WagonCardApp.Client.Extensions;
 
 public static class StringExtensions
 {
-    public static string LongHeadingFontSize(this string? text) => text is null || text.Length < 30 ? "24pt" : "20pt";
-    public static string ShortHeadingFontSize(this string? text) => text is null || text.Length < 12 ? "24pt" : $"{(int)(24 - text.Length / 2.5)}pt";
+    public static string LongHeadingStyle(this string? text) => text is null || text.Length < 25 ? "font-size: 24pt" : "fontfont-size: 20pt; font-family: Arial Narrow";
+    public static string ShortHeadingStyle(this string? text) => text is null || text.Length < 12 ? "font-size: 24pt" : $"font-size: {(int)(24 - text.Length / 3.9)}pt; font-family: Arial Narrow";
     public static string WagonNumberFontSize(this string? text) => text is null || text.Length < 12 ? "20pt" : $"{18 - text.Length / 5}pt";
 
     public static string FredStickerOperatorFontSize(this string? text) => text is null || text.Length < 6 ? "16pt" : $"{18 - text.Length * 1}pt";
